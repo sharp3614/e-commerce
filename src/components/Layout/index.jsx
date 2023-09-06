5e89f96c6cb0780d266dcea6ede3eadca5f6038f
sharp3614 <knurhak09@gmail.com>
2023-09-06 11:38:17 +0300

    Added layout

---
+++ src/components/Layout/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Sidebar from "../Sidebar";
+
+const Layout = ({ children }) => {
+  return (
+    <div className="h-screen w-screen flex p-4">
+      <div className="flex items-center">
+        <Sidebar />
+      </div>
+      <div className="pt-2 px-2">{children}</div>
+    </div>
+  );
+};
+
+export default Layout;
